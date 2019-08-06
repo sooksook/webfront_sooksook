9dad269083c5d0633e7e9e49569fe3bd114a0902
Kimjiwon <jhd1925@gmail.com>
2019-08-06 22:55:25 +0900

    feat: detail page markup

--- src/components/Detail/index.tsx
+++ src/components/Detail/index.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
 
-export default function DetailView() {
-  return <div>디테일페이지</div>;
+import { Plant } from '../../containers/Detail';
+import fakeImg from '../../100228.jpg';
+
+interface IProps {
+  plant: Plant;
 }
+
+type PropNames =
+  | 'difficulty'
+  | 'light'
+  | 'temperature'
+  | 'place'
+  | 'water'
+  | 'soil'
+  | 'fertilizer'
+  | 'breeding';
+
+const propNames = {
+  difficulty: '난이도',
+  light: '햇빛',
+  temperature: '온도',
+  place: '장소',
+  water: '물주기',
+  soil: '흙',
+  fertilizer: '비료',
+  breeding: '번식'
+};
+
+const plantDetailCard = (prop: PropNames, data: string) => {
+  if (!data) return;
+  return (
+    <li>
+      <h3>{propNames[prop]}</h3>
+      <p>{data}</p>
+    </li>
+  );
+};
+
+const DetailView: React.FC<IProps> = (props) => {
+  const detailData = Object.keys(props.plant);
+
+  const nameIndex = detailData.indexOf('name');
+  detailData.splice(nameIndex, 1);
+
+  const categoryIndex = detailData.indexOf('category');
+  detailData.splice(categoryIndex, 1);
+
+  return (
+    <main>
+      <h1>{props.plant.name}</h1>
+      <span>#{props.plant.category}</span>
+      <img src={fakeImg} alt={props.plant.name} />
+      <h2>{props.plant.name} 상세정보</h2>
+      <ul>
+        {detailData.map((data) =>
+          plantDetailCard(data as PropNames, props.plant[data as PropNames])
+        )}
+      </ul>
+    </main>
+  );
+};
+
+export default DetailView;
--- src/containers/Detail.tsx
+++ src/containers/Detail.tsx
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import DetailView from '../components/Detail';
 
-export default class Detail extends Component {
-  componentDidMount() {}
+export interface Plant {
+  name: string; // 이름
+  category: string; // 카테고리
+  difficulty: string; // 난이도
+  light: string; // 햇빛
+  temperature: string; // 온도
+  place: string; // 장소
+  water: string; // 물주기
+  soil: string; // 흙
+  fertilizer: string; // 비료
+  breeding: string; // 번식
+}
+interface IProps extends RouteComponentProps {}
+
+interface IState {
+  plant: Plant;
+}
+
+const fakeData: Plant = {
+  name: '꽃베고니아',
+  category: '공기정화식물',
+  difficulty: '키우기 쉬운 편이에요.',
+  light: '햇빛을 좋아해요.\n햇빛을 많이 받으면 잎과 꽃의 색이 진해져요.',
+  temperature: '10 - 20도 정도가 좋아요.\n더위와 추위에 모두 약해요.',
+  place: '바람이 잘 드는 곳이 좋아요.',
+  water: '겉흙이 말랐을 때 충분히 주세요.\n과한 습도에는 약하니 주의해주세요.',
+  soil: '물이 잘빠지는 흙이 좋아요.',
+  fertilizer: '',
+  breeding: '삽목(꺾꽂이, 가지를 잘라 심는 방법)\n종자(씨앗)\n포기나누기'
+};
+
+class Detail extends Component<IProps, IState> {
+  state = {
+    plant: {
+      name: '', // 이름
+      category: '', // 카테고리
+      difficulty: '', // 난이도
+      light: '', // 햇빛
+      temperature: '', // 온도
+      place: '', // 장소
+      water: '', // 물주기
+      soil: '', // 흙
+      fertilizer: '', // 비료
+      breeding: ''
+    }
+  };
+
+  componentDidMount() {
+    this.setState({
+      plant: fakeData
+    });
+  }
 
   render() {
-    return <DetailView />;
+    return <DetailView plant={this.state.plant} />;
   }
 }
+
+export default withRouter(Detail);
